fcca371a7512a9c0f7653893989dba5fcc6f3efb
phattran <phattran052004@gmail.com>
2026-04-26 11:18:37 +0700

    update gitignore

--- src/config/utils.js
+++
@@ -1,4 +0,0 @@
-// export const BASE_URL = "http://localhost:5000/api/v1";
-// export const SOCKET_URL = "http://localhost:5000";
-export const BASE_URL = "https://social-network-be-4sf1.onrender.com/api/v1";
-export const SOCKET_URL = "https://social-network-be-4sf1.onrender.com";
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,15 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { store, persistor } from "./redux/store.js";
-import NavLinkProvider from "./context/NavLinkContext.jsx";
-import ThemeProvider from "./context/ThemeContext.jsx";
-import OpenProvider from "./context/OpenContext.jsx";
-import IsEmptyProvider from "./context/IsEmptyContext.jsx";
-import SocketProvider from "./context/SocketContext.jsx";
-import AxiosProvider from "./context/AxiosContext.jsx";
-import App from "./App.jsx";
+import NavLinkProvider from "@/context/NavLinkContext.jsx";
+import ThemeProvider from "@/context/ThemeContext.jsx";
+import OpenProvider from "@/context/OpenContext.jsx";
+import IsEmptyProvider from "@/context/IsEmptyContext.jsx";
+import SocketProvider from "@/context/SocketContext.jsx";
+import { AxiosProvider } from "@/context/AxiosContext.jsx";
+import App from "@/App.jsx";
 
-import "./index.scss";
+import "@/index.scss";
 import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
